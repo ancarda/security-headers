fcf4a02bdecefdcfb50e5903e301687ec78da9bd
Mark Dain <mark@markdain.net>
2017-12-02 16:58:51 +0000

    Tighten up tests for FrameOptions
    The unit tests for the FrameOptions class should use assertEquals, not
    assertContains, as the test is suppose to be checking the entirety of
    the returned string, rather than a portion of it.
    
    These tests were based off the tests for the ContentSecurityPolicy
    class, which uses assertContains given how long and complex the
    generated header can be.

--- tests/FrameOptionsTest.php
+++ tests/FrameOptionsTest.php
@@ -12,31 +12,31 @@ final class FrameOptionsTest extends TestCase
     public function testBlank()
     {
         $xfo = new FrameOptions;
-        $this->assertContains("DENY", $xfo->compile());
+        $this->assertEquals("DENY", $xfo->compile());
     }
 
     public function testSameOrigin()
     {
         $xfo = new FrameOptions;
         $xfo = $xfo->withAllowFromSelf();
-        $this->assertContains("SAMEORIGIN", $xfo->compile());
+        $this->assertEquals("SAMEORIGIN", $xfo->compile());
     }
 
     public function testAllowFromURI()
     {
         $xfo = new FrameOptions;
         $xfo = $xfo->withAllowFrom('http://example.com/');
-        $this->assertContains("ALLOW-FROM http://example.com/", $xfo->compile());
+        $this->assertEquals("ALLOW-FROM http://example.com/", $xfo->compile());
     }
 
     public function testAllowFromDomain()
     {
         $xfo = new FrameOptions;
         $xfo = $xfo->withAllowFrom('example.com/');
-        $this->assertContains("ALLOW-FROM http://example.com/", $xfo->compile());
+        $this->assertEquals("ALLOW-FROM http://example.com/", $xfo->compile());
         $xfo = $xfo->withAllowFrom('http://example.com');
-        $this->assertContains("ALLOW-FROM http://example.com/", $xfo->compile());
+        $this->assertEquals("ALLOW-FROM http://example.com/", $xfo->compile());
         $xfo = $xfo->withAllowFrom('example.com');
-        $this->assertContains("ALLOW-FROM http://example.com/", $xfo->compile());
+        $this->assertEquals("ALLOW-FROM http://example.com/", $xfo->compile());
     }
 }
